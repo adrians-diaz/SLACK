a18c97d07b3dd350b9c9572397259967d04bfe2b
adrians-diaz <81953658+adrians-diaz@users.noreply.github.com>
2022-05-09 20:34:10 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./style/style.css">
+</head>
+<body>
+<header>
+
+</header>
+<main>
+
+</main>
+<footer>
+
+</footer>
+<script type="application/javascript">
+
+</script>
+
+</body>
+</html>
